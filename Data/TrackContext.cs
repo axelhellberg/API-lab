7882c93f54bcc771630b334d4051659837db136e
axelhellberg <axhe1901@student.miun.se>
2021-02-18 15:38:18 +0100

    Add project files.

---
+++ Data/TrackContext.cs
@@ -0,0 +1,19 @@
+﻿using API_lab.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_lab.Data
+{
+    public class TrackContext:DbContext
+    {
+        public TrackContext(DbContextOptions<TrackContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Track> Tracks { get; set; }
+    }
+}
---
+++ Migrations/20210218111115_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API_lab.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tracks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Artist = table.Column<string>(type: "TEXT", nullable: true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Length = table.Column<int>(type: "INTEGER", nullable: false),
+                    Genre = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tracks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tracks");
+        }
+    }
+}
